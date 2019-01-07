a0a4cbb4b2049dfe395174e9f27d27b1a495dfbf
Raid Yimne <yimne.raid@outlook.com>
2019-01-07 12:27:14 +0100

    first attempt

--- caravan.cpp
+++ caravan.cpp
@@ -10,34 +10,64 @@
  * <your description here>
  *-----------------------------------------------------------------------------
 */
+#include "caravan.h"
+#include "stdlib.h"
+typedef struct _node{
+  PackAnimal pack_animal;
+  struct _node* next;
+}Node;
+
+struct CaravanImplementation{
+  Node* head;
+};
+
+struct PackAnimalImplementation {
+	AnimalType type;
+	const char *name;
+	int max_speed;
+	int load;
+	Caravan caravan;
+};
+
 Caravan new_caravan()
 {
+  Caravan caravan = (Caravan)malloc(sizeof(struct CaravanImplementation));
+  caravan->head = 0;
+  return caravan;
 }
 
 int get_length(Caravan caravan)
 {
+
+  return 0;
 }
 
 void delete_caravan(Caravan caravan)
 {
+  return;
 }
 
 void add_pack_animal(Caravan caravan, PackAnimal animal)
 {
+  return;
 }
 
 void remove_pack_animal(Caravan caravan, PackAnimal animal)
 {
+  return;
 }
 
 int get_caravan_load(Caravan caravan)
 {
+  return 0;
 }
 
 void unload(Caravan caravan)
 {
+  return;
 }
 
 int get_caravan_speed(Caravan caravan)
 {
+  return 0;
 }
